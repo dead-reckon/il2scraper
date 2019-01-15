b34d26ddf64ea3361b490f1624996702b81f26a1
JB <jb@unitesystem.com>
2019-01-15 14:08:12 -0600

    save

--- info_scraper.py
+++ info_scraper.py
@@ -10,29 +10,32 @@ raw_html = open('source.html',encoding="utf8").read()
 
 html = BeautifulSoup(raw_html, 'html.parser')
 
-head_tag = html.head
-
-title_tag = head_tag.contents[1]
-
-
-# print(title_tag)
-# print(head_tag.contentss)
-
-
-
 content = html.find("div", {"class": "cPost_contentWrap ipsPad"})
 
 # all_plane = html.body.find_all("div", {"class": "ipsSpoiler"})
 
 first_plane = content.span.parent.find_next('p').find_next('p')
 
-print(first_plane.text.strip())
+# Find the plane
+# plane = content.find("div", {"class": "ipsSpoiler"}).find_previous_sibling('p').find_previous_sibling('p')
 
-first_content = content.find("div", {"class": "ipsSpoiler_contents"})
+# Find all Planes
+plane = content.find_all("div", {"class": "ipsSpoiler"}).find_next_siblings()
 
-for line in first_content.stripped_strings:
+
+
+for line in plane:
     print(line)
 
+# for line in plane:
+#     print(line.find_previous_sibling('p').find_previous_sibling('p'))
+
+# [ print(line) for line in plane.stripped_strings ]
+
+# first_content = content.find("div", {"class": "ipsSpoiler_contents"})
+
+# [ print(line) for line in first_content.stripped_strings ]
+
 # Check 
-# check_content = dir(content.span)
-# [ print(line) for line in check_content ]
+check_content = dir(content.span)
+[ print(line) for line in check_content ]
