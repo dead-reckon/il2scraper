1ea032de2ba2bfc13947ac1ff9d08b9a19e1a334
JB <jb@unitesystem.com>
2020-06-15 21:01:22 -0500

    custom photo push tool

---
+++ custom_photo.py
@@ -0,0 +1,23 @@
+import os
+from shutil import copyfile, move
+
+src_dir = "X:/SteamLibrary/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/data/graphics/Planes"
+
+
+def custom_photo():
+    path = os.getcwd()
+    tgt_photo = path + "/custom_photo.dds"
+    lst_plane_dirs = os.listdir(src_dir)
+
+    for line in lst_plane_dirs:
+        plane_path = src_dir + "/" + line + "/Textures/"
+        src_photo = plane_path + "custom_photo.dds"
+        src_backup = plane_path + "custom_photo_backup.dds"
+
+        # print(src_photo)
+        if os.path.exists(src_photo):
+            move(src_photo, src_backup)
+            
+        copyfile(tgt_photo, src_photo)
+
+custom_photo()
